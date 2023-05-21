61c5ebb636dfc6c26eaa583e39d5cb4195067d57
TsuraEvgesha <primak.evgesha@mail.ru>
2023-05-21 22:35:56 +0700

    Homework

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.netology.statsview.ui.StatsView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         findViewById<StatsView>(R.id.statsView).data = listOf(
-            0.25F,
-            0.25F,
-            0.25F,
-            0.25F,
-            0.25F,
+            500F,
+            500F,
+            500F,
+
+
         )
     }
 }
--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -1,24 +1,22 @@
 package ru.netology.statsview.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PointF
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.withStyledAttributes
 import ru.netology.statsview.R
 import ru.netology.statsview.utils.AndroidUtils
-import java.lang.Integer.min
-import java.util.Random
+
+
 import kotlin.random.Random.Default.nextInt
 
 class StatsView @JvmOverloads constructor(
     context:Context,
     attributeSet: AttributeSet? = null,
     defStyleAttr: Int =0,
-    defStyleRes: Int =0
+    defStyleRes: Int =0,
 ): View(
     context,
     attributeSet,
@@ -26,39 +24,40 @@ class StatsView @JvmOverloads constructor(
     defStyleRes
 ) {
     private var radius = 0F
-    private var center = PointF()
-    private var oval = RectF()
-    private var paint= Paint(
-        Paint.ANTI_ALIAS_FLAG
-    ).apply {
-        strokeWidth =lineWidth.toFloat()
-        style=Paint.Style.STROKE
-        strokeJoin = Paint.Join.ROUND
-        strokeCap = Paint.Cap.ROUND
-    }
-    private var textPaint= Paint(
-        Paint.ANTI_ALIAS_FLAG
-    ).apply {
-        textSize = this@StatsView.textSize
-        style=Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-    }
-    private var textSize = AndroidUtils.dp(context, 50).toFloat()
+    private var center = PointF(0F,0F)
+    private var oval = RectF(0F,0F,0F,0F)
+
+    private var textSize = AndroidUtils.dp(context, 40).toFloat()
     private var lineWidth = AndroidUtils.dp(context,5)
     private var colors = emptyList<Int>()
+    private var step = 0F
 
     init {
       context.withStyledAttributes(attributeSet, R.styleable.StatsView){
           textSize=getDimension(R.styleable.StatsView_textSize, textSize)
           lineWidth=getDimension(R.styleable.StatsView_lineWidth, lineWidth.toFloat()).toInt()
-          colors=listOf (
-          getColor(R.styleable.StatsView_color1, generateRandomColor()),
-          getColor(R.styleable.StatsView_color2, generateRandomColor()),
-          getColor(R.styleable.StatsView_color3, generateRandomColor()),
-          getColor(R.styleable.StatsView_color4, generateRandomColor()),
-              getColor(R.styleable.StatsView_color5, generateRandomColor()),
-          )
+          val resId = getResourceId(R.styleable.StatsView_colors, 0)
+          colors= resources.getIntArray(resId).toList()
       }
+    }
+    private var paint= Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth =lineWidth.toFloat()
+        style=Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+    private var freePaint= Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth =lineWidth.toFloat()
+        style=Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        color= Color.LTGRAY
+
+    }
+    private var textPaint= Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = this@StatsView.textSize
+        style=Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
     }
     var data: List<Float> = emptyList()
     set(value) {
@@ -66,37 +65,49 @@ class StatsView @JvmOverloads constructor(
         invalidate()
     }
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        radius=min(w,h)/2F - lineWidth
-        center = PointF(w/2F,h/2F)
+        radius= kotlin.math.min(w, h) / 2F - lineWidth/ 2
+        center = PointF(w / 2F,h / 2F)
         oval = RectF(
-            center.x - radius,
-            center.y - radius,
-            center.x + radius,
-            center.y + radius
+            center.x - radius, center.y - radius,
+            center.x + radius, center.y + radius
         )
 
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onDraw(canvas: Canvas) {
         if(data.isEmpty()){
             return
         }
+        canvas.drawCircle(center.x,center.y,radius, freePaint)
         var startAngle = -90F
-        data.forEachIndexed { index, datum ->
-            val angle = datum * 360F
-            paint.color = colors.getOrElse(index) {generateRandomColor()}
+        for ((index, datum) in data.withIndex()) {
+            val angle = 1/((data.sum()/datum)+1)*360F
+            paint.color = colors.getOrNull(index) ?: generateRandomColor()
             canvas.drawArc(oval, startAngle, angle, false, paint)
             startAngle += angle
+            step=angle+startAngle
+
         }
+
+
+//        второе задание
+//        if (data.sum()>=1F){
+//            val angle = 1F
+//            paint.color = colors[4]
+//            canvas.drawArc(oval, startAngle,angle,false, paint)
+//        }
+
+
         canvas.drawText(
-            "%.0f%%".format(data.sum() * 100),
+            "%.2f%%".format(step/360*100),
             center.x,
             center.y + textPaint.textSize / 4,
-            textPaint
+            textPaint,
         )
+
     }
 
-    private fun generateRandomColor() =
-        nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+    private fun generateRandomColor() = nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 
 }
